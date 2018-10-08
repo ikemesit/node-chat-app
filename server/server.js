827866dcd07847b51a735fa20243bb5ec572ba1b
ikemesit <ikemesit@gmail.com>
2018-10-09 02:24:23 +0800

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -13,6 +13,18 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New User connected');
+
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to our chat group',
+        createdAt: new Date().getTime()
+    }); 
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'A new user joined',
+        createdAt: new Date().getTime()
+    });
     
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
@@ -21,6 +33,7 @@ io.on('connection', (socket) => {
             text: message.text,
             createdAt: new Date().getTime()
         });
+
     });
 
     socket.on('disconnect', () => {
