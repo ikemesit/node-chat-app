273709c2a16d20929ebcaff4f63cb66b063cc7df
ikemesit <ikemesit@gmail.com>
2018-10-09 02:01:59 +0800

    Add broadcast newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ const socket = io();
 
 socket.on('connect', () => {
     console.log('connected to server');
-
-    socket.emit('createMessage', {
-        to: 'mo@example.com',
-        text: 'Hey, get over here!!'
-    });
 });
 
 socket.on('disconnect', () => {
