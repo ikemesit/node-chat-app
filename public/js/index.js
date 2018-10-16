cabb695af2c7ca2d82c45c10e18276cb71f70c23
ikemesit <ikemesit@gmail.com>
2018-10-16 12:06:42 +0800

    add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -10,4 +10,25 @@ socket.on('disconnect', () => {
 
 socket.on('newMessage', (message) => {
     console.log('new message', message);
+    const li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    jQuery('#messages').append(li);
 });
+
+// socket.emit('createMessage', {
+//     from: 'Tony',
+//     text: 'hello!'
+// }, (data) => {
+//     console.log('Got it!', data);
+// });
+
+jQuery('#message-form').on('submit', (e) => {
+    e.preventDefault();
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, () => {
+
+    })
+})
